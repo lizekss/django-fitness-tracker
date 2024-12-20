bdb29b2ee3a5cf34cc4f5b007d12a554ad3c869f
lizekss <lekse20@freeuni.edu.ge>
2024-12-20 16:56:47 +0400

    update permissions in wellness app

--- wellness/views.py
+++ wellness/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import FitnessActivity, Meal, SleepLog
+from .permissions import IsOwner
 from .serializers import FitnessActivitySerializer, MealSerializer, SleepLogSerializer
 
 class FitnessActivityViewSet(viewsets.ModelViewSet):
     serializer_class = FitnessActivitySerializer
     queryset = FitnessActivity.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -14,7 +15,7 @@ class FitnessActivityViewSet(viewsets.ModelViewSet):
 class MealViewSet(viewsets.ModelViewSet):
     serializer_class = MealSerializer
     queryset = Meal.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -22,7 +23,7 @@ class MealViewSet(viewsets.ModelViewSet):
 class SleepLogViewSet(viewsets.ModelViewSet):
     serializer_class = SleepLogSerializer
     queryset = SleepLog.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
