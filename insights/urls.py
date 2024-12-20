eb8badccd660af737bc3b7bdc883a3d86d824e8a
lizekss <lekse20@freeuni.edu.ge>
2024-12-20 17:08:47 +0400

    extend weekly report to daily, monthly + custom

--- insights/urls.py
+++ insights/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import WeeklyReportView
+from .views import WeeklyReportView, CustomReportView, MonthlyReportView, DailyReportView
 
 urlpatterns = [
+    path('daily-report/', DailyReportView.as_view(), name='daily-report'),
     path('weekly-report/', WeeklyReportView.as_view(), name='weekly-report'),
+    path('monthly-report/', MonthlyReportView.as_view(), name='monthly-report'),
+    path('report/', CustomReportView.as_view(), name='custom-report'),
 ]
--- insights/views.py
+++ insights/views.py
@@ -1,14 +1,59 @@
+from abc import ABC, abstractmethod
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .utils import generate_personalized_insights
 
-class WeeklyReportView(APIView):
+
+
+class ReportView(APIView, ABC):
     """
-    API endpoint to generate and return weekly personalized insights for the user.
+    Abstract parent class for generating reports based on the number of days.
     """
     permission_classes = [IsAuthenticated]
 
+    @abstractmethod
+    def get_days(self):
+        pass
+
     def get(self, request, *args, **kwargs):
-        insights = generate_personalized_insights(request.user)
+        days = self.get_days()
+        insights = generate_personalized_insights(request.user, days)
         return Response(insights)
+
+
+class DailyReportView(ReportView):
+    """
+    API endpoint to generate and return daily personalized insights for the user.
+    """
+    def get_days(self):
+        return 1
+
+
+class WeeklyReportView(ReportView):
+    """
+    API endpoint to generate and return weekly personalized insights for the user.
+    """
+    def get_days(self):
+        return 7
+
+
+class MonthlyReportView(ReportView):
+    """
+    API endpoint to generate and return monthly personalized insights for the user.
+    """
+    def get_days(self):
+        return 30
+
+
+class CustomReportView(ReportView):
+    """
+    API endpoint to generate and return a custom personalized report based on the number of days.
+    """
+    def get_days(self):
+        days = self.request.query_params.get('days')
+        try:
+            days = int(days)
+        except (TypeError, ValueError):
+            days = 7  # Default to 7 if the 'days' param is invalid or missing
+        return days
